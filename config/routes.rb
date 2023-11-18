386f5e5b05a56ed0fe2d7e44af16aaee1c7a5bc8
Nicolai <Nicolaigorden@gmail.com>
2023-11-18 14:33:31 -0500

    user model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get '/hello', to: 'application#hello_world'
 
   get '*path',
-  to: 'fallback#index',
-  constraints: ->(req) { !req.xhr? && req.format.html? }
+    to: 'fallback#index',
+    constraints: ->(req) { !req.xhr? && req.format.html? }
 end
