7fcef7d5b9eb5750c581a1c79cc3be72f036d76c
Nicolai <Nicolaigorden@gmail.com>
2023-11-18 23:46:22 -0500

    user index, create, serializer}

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  #USERS
+  resources :users, only: [:index, :create, :show]
+  
   get '*path',
     to: 'fallback#index',
     constraints: ->(req) { !req.xhr? && req.format.html? }
