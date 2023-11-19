b8b0bfb7f6c5ade52b6efaf34f27849199e9576e
Nicolai <Nicolaigorden@gmail.com>
2023-11-18 23:17:52 -0500

    navbar navigation

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from '../Styles/Navbar.css'
 
 function Navbar() {
+
+    const navigate = useNavigate();
+
+    function toLogin(e) {
+        e.preventDefault()
+        navigate('/gate')
+    }
+
+    function toSignUp(e) {
+        e.preventDefault()
+        navigate('/gate/signup')
+    }
     return (
         <header>
             <nav>
                 <h1 className="title">QwikReview</h1>
                 <ul className="Navul">
-                    <li className="Navli signup"><button className="navbutton">SIGN UP</button></li>
-                    <li className="Navli login"><button className="navbutton">LOGIN</button></li>
+                    <li className="Navli signup"><button onClick={toSignUp} className="navbutton">SIGN UP</button></li>
+                    <li className="Navli login"><button onClick={toLogin} className="navbutton">LOGIN</button></li>
                 </ul>
             </nav>
         </header>
